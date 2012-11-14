430948865e4527b5fa3e5a05a04a6667be8e7836
Ricardo Bánffy <rbanffy@gmail.com>
2012-11-14 00:35:36 -0200

    added extra controls for subject and test

--- src/s3cr3754n74-python/main.py
+++ src/s3cr3754n74-python/main.py
@@ -21,24 +21,32 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write(template.render({}))
 
     def post(self):
-        # pdb.set_trace()
+        # import pdb; pdb.set_trace()
         emails = self.request.POST['emails'].split()
         random.shuffle(emails)
         sender_address = "Amigo secreto "\
             "<no-reply@banffy.com.br>"
-        subject = "Amigo secreto - Valendo"
+        subject = self.request.POST['subject']
+        just_test = self.request.POST.get('just_test', False) == 'true'
         email_template = jinja_environment.get_template('email.txt')
         response_template = jinja_environment.get_template('response.html')
-        for i in range(len(emails)):
-            body = email_template.render({'kid': emails[i]})
-            user_address = emails[i-1]
-            try:
-                mail.send_mail(sender_address, user_address, subject, body)
-            except mail.InvalidSenderError:
-                logging.error('Invalid sender error %s' % sender_address)
-                self.abort(500)
-                # logging.error('invalid e-mail %s' % user_address)
-        self.response.write(response_template.render({'emails': emails}))
+        if just_test:
+            messages = []
+            for i in range(len(emails)):
+                messages.append('%s will gift %s' % (emails[i-1], emails[i]))
+            output = response_template.render({'emails': messages})
+        else:
+            for i in range(len(emails)):
+                body = email_template.render({'kid': emails[i]})
+                user_address = emails[i-1]
+                try:
+                    mail.send_mail(sender_address, user_address, subject, body)
+                except mail.InvalidSenderError:
+                    logging.error('Invalid sender error %s' % sender_address)
+                    self.abort(500)
+                    # logging.error('invalid e-mail %s' % user_address)
+            output = response_template.render({'emails': emails})
+        self.response.write(output)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
